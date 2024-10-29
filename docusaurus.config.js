dd8e6b7231d938b898d6556ea57d526c8660f8a8
kaindraa <143985776+kaindraa@users.noreply.github.com>
2024-10-29 09:18:22 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: '242saurus',
+  tagline: 'kaindraa loves docusaurus',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
