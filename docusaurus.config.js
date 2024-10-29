a5a07ce8cf5e140208d3fa35e8ecb90e45989bb0
kaindraa <143985776+kaindraa@users.noreply.github.com>
2024-10-29 08:30:40 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,6 +22,8 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
+  deploymentBranch : 'gh-pages'
+
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
